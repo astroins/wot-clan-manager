789ef2257d1fdbe9911c649a1c2929cd1909d7b4
Andrzej Stroiński <andrzej.stroinski@cs.put.poznan.pl>
2015-11-17 20:12:02 +0100

    some changes 4 testing

---
+++ source/wot-clan-manager-v0.1/wot-cm.py
@@ -0,0 +1,44 @@
+__author__ = 'Andrzej Stroiński'
+# all the imports
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+
+# configuration
+DATABASE = './db/wot-cm.db'
+DEBUG = True
+SECRET_KEY = 'development key'
+USERNAME = 'admin'
+PASSWORD = 'default'
+
+# create our little application :)
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+
+
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+@app.before_request
+def before_request():
+    g.db = connect_db()
+
+
+@app.teardown_request
+def teardown_request(exception):
+    db = getattr(g, 'db', None)
+    if db is not None:
+        db.close()
+
+
+print(__name__)
+
+if __name__ == '__main__':
+    app.run()globals()
---
+++ source/wot-clan-manager-v0.1/wot.py
@@ -0,0 +1,52 @@
+import requests
+import json
+import datetime	
+from flask import Flask
+
+
+def get_account_id(nick):
+
+    r = requests.get('https://api.worldoftanks.eu/wot/account/list/?application_id=demo&search=' + nick)
+    data = json.loads(r.text)
+    acc_id = data["data"][0]["account_id"]
+
+    return acc_id
+
+
+def member_since_days(account_id):
+    r = requests.post("https://api.worldoftanks.eu/wgn/clans/membersinfo/?application_id=demo&account_id=" + str(account_id))
+    data = json.loads(r.text)
+    timestamp = data["data"][str(account_id)]["joined_at"]
+    clan_join_date = datetime.datetime.fromtimestamp(int(str(timestamp)))
+    current_date = datetime.datetime.now()
+    delta = current_date - clan_join_date
+
+    return delta.days
+
+
+def get_total_resources(account_id):
+
+    r = requests.get("https://api.worldoftanks.eu/wot/stronghold/accountstats/?application_id=demo&account_id=" + str(account_id))
+    data = json.loads(r.text)
+    total_resources_earned = data["data"][str(account_id)]["total_resources_earned"]
+
+    return total_resources_earned
+
+account_id = get_account_id("reibenus")
+print(account_id)
+days = member_since_days(account_id)
+print(days)
+total_resources_earned = get_total_resources(account_id)
+print(total_resources_earned / days)
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+print(__name__)
+
+if __name__ == '__main__':
+    app.run()
